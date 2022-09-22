6b32b73b05bbe37b894e638b3cef7a6814af671a
Fabio German Palacios <gfpalacios@gmail.com>
2022-09-21 23:28:57 -0400

    my changes

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -97,9 +97,9 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: `I am a driven engineer and entrepreneur who likes trying to solve big problems in innovative ways. I have applied these skills in both small and large companies, 
+  including Avaya Inc, Thales Group (Ex Gemalto), having shipped multiple successful projects in both business and consumer-facing markets. 
+  I have great experience in both Back end and Front end as full-stack web developer`,
   aboutItems: [
     {label: 'Location', text: 'Montreal, QC, Canada', Icon: MapIcon},
     {label: 'Nationality', text: 'Argentinian / Italian', Icon: FlagIcon},
@@ -114,23 +114,6 @@ export const aboutData: About = {
  * Skills section
  */
 export const skills: SkillGroup[] = [
-/*   {
-    name: 'Spoken languages',
-    skills: [
-      {
-        name: 'Spanish',
-        level: 10,
-      },
-      {
-        name: 'English',
-        level: 9,
-      },
-      {
-        name: 'Italian',
-        level: 1,
-      },
-    ],
-  }, */
   {
     name: 'Frontend development',
     skills: [
@@ -194,11 +177,11 @@ export const skills: SkillGroup[] = [
 export const portfolioItems: PortfolioItem[] = [
   {
     title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://timbaker.me',
+    description: 'TBD',
+    url: 'https://www.linkedin.com/in/gfpalacios',
     image: porfolioImage1,
   },
-  {
+  /* {
     title: 'Project title 2',
     description: 'Give a short description of your project here.',
     url: 'https://timbaker.me',
@@ -250,14 +233,12 @@ export const portfolioItems: PortfolioItem[] = [
     title: 'Project title 10',
     description: 'Give a short description of your project here.',
     url: 'https://timbaker.me',
-    image: porfolioImage10,
-  },
-  {
+    image: porfolioImageNational Technology University
     title: 'Project title 11',
     description: 'Give a short description of your project here.',
     url: 'https://timbaker.me',
     image: porfolioImage11,
-  },
+  }, */
 ];
 
 /**
@@ -265,40 +246,211 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'December 2013',
+    location: 'UBA (Buenos Aires University)',
+    title: 'Master in Engineering Entrepreneurship',
+    content: <p></p>,
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: 'December 2008',
+    location: 'UTN (National Technological University - FRBA)',
+    title: 'Information Systems Engineer',
+    content: <p></p>,
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: 'January 2018 – - Present',
+    location: 'MindGeek - Montreal, Canada',
+    title: 'Senior Web Developer',
     content: (
+      <>
+      <ul>
+        <li>
+        &#8226; Developed new features for a web application in React which provide media content to millions of users from early stages. High traffic and SSR requirements.
+        </li>
+        <li>
+        &#8226; Implemented new features and maintenance for an admin web site in React.
+        </li>
+        <li>
+        &#8226; Participated in development tasks as core team member of one of the biggest areas of engineering in the company.
+        </li>
+        <li>
+        &#8226; Designed and implemented a testing framework in Jest which accelerated 100% unit test development and 90% of code coverage.
+        </li>
+        <li>
+        &#8226; Trained over 20 staff member in unit test for frontend, including steps on how to use Jest and Enzyme properly.
+        </li>
+      </ul>
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        <b>Technologies:</b> Javascript, React, Redux, Express, Jest, Enzyme.
       </p>
+      </>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: 'May 2016 – January 2019',
+    location: 'Qomplx Inc. - Virginia, USA',
+    title: 'Senior Software Engineer / Scala Developer',
     content: (
-      <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
-      </p>
+      <ul>
+        <li>
+        &#8226; Developed rest api and backend features for a web solution which applies artificial
+intelligence to predict the stock market behavior, buy/sell stocks based on complex models
+and data analysis in real time. Scala, Play, Akka, Slick.
+        </li>
+        <li>
+        &#8226; Developed new end to end features for a web application scala/vue.js to handle apache
+spark models.
+        </li>
+        <li>
+        &#8226; Participated as full stack Scala Developer in the core web application development. Scala,
+liftweb, Squeryl, Spray, Akka.
+        </li>
+        <li>
+        &#8226; Developed two UI editors to manage visual components into a Canvas. Javascript, Angular.
+        </li>
+        <li>
+        &#8226; AWS Collector developed. A standalone process which consumes Amazon API to retrieve
+periodically pre-configured AWS instance data of the end user.
+        </li>
+        <li>
+        &#8226; Modified and Updated Spring Social libraries to adapt them to the Fractal Business model.
+        </li>
+        <li>
+        <b>Technologies:</b> Scala, liftweb, Squeryl, Slick, Play framework, Spray, Akka, MySql,MongoDB, Docker, AWS, Javascript, Angular, Java, Spring, Maven.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    date: 'May 2012 – January 2018',
+    location: 'Avaya Inc - Buenos Aires, Argentina',
+    title: 'Senior Software Engineer',
+    content: (
+      <ul>
+        <li>
+        &#8226; Developed code to solve complex technical requirements related to a web administration
+tool which allows the creation and management of call flows easily and quickly.
+        </li>
+        <li>
+        &#8226; Researched different JavaScript technologies focused on intuitive and user friendly
+interfaces.
+        </li>
+        <li> Developed a new reporting tool using ELK (Elasticsearch, Logstash and Kibana).
+        &#8226;
+        </li>
+        <li>
+        &#8226; Implemented Scala micro service. Designed and developed Java API Rest services.
+        </li>
+        <li>
+        &#8226; Wrote Code to add new functionality to a Social Media Manager product that integrates
+Avaya contact center with different social networks like Facebook, Twitter, YouTube, etc.
+        </li>
+        <li>
+        &#8226; Researched and developed BI application to build tabular reports, dashboards and
+maintenance of call flows generator application that allows contact center managers make
+decisions.
+        </li>
+        <li>
+        &#8226; Created automation test, integration test, load and stress test for web based and BI
+applications.
+        </li>
+        <li>
+        &#8226; Performed continuous improvement, continuous integration, and continuous inspection
+activities.
+        </li>
+        <li>
+        &#8226; Participated in Agile development process. Multicultural teams.
+        </li>
+        <li>
+        &#8226; Wrote scripts in Linux based platforms in order to setup, maintain and optimize
+environments.
+        </li>
+        <li>
+        &#8226; Managed systems configuration, virtual machines and test system environments.
+        </li>
+        <li>
+        &#8226; Provided support to internal and external customers for complex technical issues.
+        </li>
+        <li>
+        &#8226; Organized sessions which helped in knowledge transfer across the team and internal
+customers.
+        </li>
+        <li>
+        &#8226; Documented user guides and tutorials.
+        </li>
+        <li>
+        <b>Technologies:</b> Java, JavaScript, Groovy, Scala, Angular, Jquery, Backbone.js, Joint.js,
+ElasticSearch, Logstash, Kibana, Bootstrap, Bower, Karma, Jasmine, HTML, CSS, Hibernate,
+Maven, Spring, GWT, Echo2, SpringMVC, Oracle BI (OBIEE 11g), Oracle DB, PostgreSQL,
+Graphviz, Talend, SVN, Git, Jira, Nexus, Bamboo, Sonar, Crucible.
+        </li>
+      </ul>
+    ),
+  },
+  {
+    date: 'January 2009 – May 2012',
+    location: 'Gemalto - Buenos Aires, Argentina',
+    title: 'Senior Software Developer',
+    content: (
+      <ul>
+        <li>
+        &#8226; Designed architecture and implemented web application to build banking transaction
+reports to every Mexican citizen. Validated scalability, performance criteria and security
+vulnerabilities.
+        </li>
+        <li>
+        &#8226; Coached and mentored a team about 3 people. Prepared Technical classes and tutorials.
+        </li>
+        <li>
+        &#8226; Developed SIMCard related software projects for Latam customers. Interacted among
+abroad teams. 
+        </li>
+        <li>
+        &#8226; Generated new reusable components based on refactors over existent code.
+        </li>
+        <li>
+        &#8226; Developed gateway to manage STKML pages based on banking transactions.
+        </li>
+        <li>
+        &#8226; Created new type of transactions in Gemalto mobile banking product requested by
+external customers.
+        </li>
+        <li>
+        &#8226; Implemented web services in different SOA architectures.
+        </li>
+        <li>
+        &#8226; Participated in the integration of cross-organization development.
+        </li>
+        <li>
+        &#8226; Created performance measurement instrumentation used to observe application and
+infrastructure performance across application servers.
+        </li>
+        <li>
+        &#8226; Involved in continuous improvement and continuous integration processes.
+        </li>
+        <li>
+        &#8226; Promoted agile practices. Participated in the first agile development in the area.
+        </li>
+        <li>
+        &#8226; Created user guides through LaTex programming. Documented tutorials in wiki pages.
+        </li>
+        <li>
+        &#8226; Presenter in knowledge transfer meetings.
+        </li>
+        <li>
+        &#8226; Conducting technical interviews.
+        </li>
+        <li>
+        &#8226; International experience.
+        </li>
+        
+        <li>
+        <b>Technologies:</b> Scala, liftweb, Squeryl, Slick, Play framework, Spray, Akka, MySql,MongoDB, Docker, AWS, Javascript, Angular, Java, Spring, Maven.
+        </li>
+      </ul>
     ),
   },
 ];
@@ -309,7 +461,7 @@ export const experience: TimelineItem[] = [
 
 export const contact: ContactSection = {
   headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  description: 'Have a project for me? I would love to hear from you, give me a shout by email or by using the form below if you would like to get in contact with me.',
   items: [
     {
       type: ContactType.Email,
